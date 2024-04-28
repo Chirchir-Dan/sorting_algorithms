ea2a4855080998a165f8699a0033c61437d46c62
Dev-Kings <kingasiadavid41@gmail.com>
2024-04-28 17:14:50 +0300

    added checker to determine if array is sorted

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,10 +10,14 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int temp, sorted;
+
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		sorted = 1;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -21,8 +25,11 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				sorted = 0;
 				print_array(array, size);
 			}
 		}
+		if (sorted)
+			break;
 	}
 }
