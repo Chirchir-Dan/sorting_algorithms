3f6b808b63acfadb064d972fae5c164308387b4a
Chirchir-Dan <1danchirchir@gmail.com>
2024-04-30 12:43:43 +0300

    removed a header file in sort.h

--- sort.h
+++ sort.h
@@ -1,7 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
-
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -25,5 +24,7 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif /* SORT_H */
